9178da8a38c2484b05cb430a1f1f942bff654f17
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2023-12-25 07:52:06 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 # Create a Resource_Group 
 resource "azurerm_resource_group" "rg1" {
   name     = "myTFResourceGroup"
-  location = ""West Europe""
+  location = "West Europe"
 }
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
   address_space       = ["10.0.0.0/16"]
-  location            = ""West Europe""
+  location            = "West Europe"
   resource_group_name = azurerm_resource_group.rg1.name
 }
 
