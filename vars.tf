fc25b083f06432a1f2ed701bd1bb53b090e849fa
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-02 06:58:14 +0530

    vars.tf

--- vars.tf
+++ vars.tf
@@ -1,4 +1,4 @@
 variable "location" {
   description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
-location = "West Europe"
+location = "East US"
 }
