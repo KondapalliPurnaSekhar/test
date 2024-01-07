d1925e2693c45fa49da99b98c6188c26905b18bf
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-07 19:58:02 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network" "vnet" {
 # Create a sub_net network
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = var.Resource_Group
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
