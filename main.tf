150c0969ce35791ab21e4caa32da842af40a7237
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-15 21:51:02 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.Resource_Group
+depends_on = [
+    azurerm_resource_group.rg
+  ]
 }
 
 # Create a sub_net network
