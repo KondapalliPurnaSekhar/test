d8967285ad88ec8e8a376fd8c0e24dfaafab485a
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-15 18:19:49 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ resource "azurerm_resource_group" "rg" {
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   count = 4
-  name                = "var.network-${count.intex}"
-  address_space       = ["10.0.${count.intex}.0/16"]
+  name                = var.network-${count.index}
+  address_space       = ["10.0.${count.index}.0/16"]
   location            = var.location
   resource_group_name = var.Resource_Group
 }
@@ -17,9 +17,9 @@ resource "azurerm_virtual_network" "vnet" {
 # Create a sub_net network
 resource "azurerm_subnet" "subnet" {
 count = 4
-  name                 = "var.subnet-${count.intex}"
+  name                 = var.subnet-${count.index}
   resource_group_name  = var.Resource_Group
   virtual_network_name = var.network
-  address_prefixes     = ["10.0.${count.intex}.0/24"]
+  address_prefixes     = ["10.0.${count.index}.0/24"]
 }
 
