3df46c7ac8cb4ab177389f4ebf083b9a285b7988
KondapalliPurnaSekhar <36615694+KondapalliPurnaSekhar@users.noreply.github.com>
2024-01-15 17:31:40 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   
-  name                = "var.subnet"
+  name                = "var.network"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.Resource_Group
